105da3b80a48cb54e4fb9d3dc11e621eece1989c
Natanael Carvalho <natan.carvalho123@hotmail.com>
2023-10-27 13:34:49 -0300

    Update deleteUser.route.js

--- src/routes/user/deleteUser.route.js
+++ src/routes/user/deleteUser.route.js
@@ -7,29 +7,6 @@ import { authValidate } from "../../utils/auth-validate.js";
 export class DeleteUser {
   async handler(request, response) {
     try {
-<<<<<<< HEAD
-      const [, , id] = request.url.split('/')
-      const inMemoryStrategy = new InMemoryStrategy(inMemoryDB)
-      const contextStrategy = new ContextStrategy(inMemoryStrategy)
-      const deleteUser = await contextStrategy.delete(id)
-      const tokenValid = authValidate(request)
-
-      if (!tokenValid.isValid) {
-        throw new Error(tokenValid.message)
-      }
-
-      if (!deleteUser) {
-        throw new Error("user not found!")
-      }
-
-      response.writeHead(200, DEFAULT_HEADER)
-      response.write(JSON.stringify({ message: 'user deleted!' }))
-      return response.end()
-    } catch (error) {
-      response.writeHead(400, DEFAULT_HEADER)
-      response.write(JSON.stringify({ error: error.message }))
-      return response.end()
-=======
       const { id } = request.params;
       const inMemoryStrategy = new InMemoryStrategy(inMemoryDB);
       const contextStrategy = new ContextStrategy(inMemoryStrategy);
@@ -51,7 +28,6 @@ export class DeleteUser {
       response.writeHead(400, DEFAULT_HEADER);
       response.write(JSON.stringify({ error: error.message }));
       return response.end();
->>>>>>> development
     }
   }
 }
